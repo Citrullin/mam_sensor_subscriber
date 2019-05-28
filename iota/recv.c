17594e316923b6f74015b16c7778a150462d2d4b
Philipp Blum <philipp-blum@jakiku.de>
2019-05-28 11:35:59 +0200

    Use ascii_to_trytes

--- iota/recv.c
+++ iota/recv.c
@@ -55,33 +55,16 @@ int mam_receive(byte_t *payload, int *payload_size, char *host, int port, char *
         *payload_size = MIN_BYTES(payload_tryte_size * NUMBER_OF_TRITS_IN_A_TRYTE);
         payload = (byte_t*) malloc(*payload_size * sizeof(byte_t));
 
-        size_t trit_size = (payload_tryte_size * NUMBER_OF_TRITS_IN_A_TRYTE);
-        trit_t* payload_trits = (trit_t*) malloc(trit_size * sizeof(trit_t));
-
-        trytes_to_trits(payload_trytes, payload_trits, payload_tryte_size);
-
-        fprintf(stderr, "payload_size_trits: %i\n", trit_size);
-
-        fprintf(stderr, "payload: [ ");
-        for (unsigned int i = 0; i < trit_size; i++) {
-            if (i == 0) {
-                fprintf(stderr, "0x%x", payload_trits[i]);
-            } else {
-                printf(stderr, ", 0x%x", payload_trits[i]);
-            }
-        }
-        printf(" ]\n");
-
-        trits_to_bytes(payload_trits, payload, trit_size);
+        trytes_to_ascii(payload_trytes, payload_tryte_size, payload);
 
         fprintf(stderr, "payload_size: %i\n", *payload_size);
 
         fprintf(stderr, "payload: [ ");
         for (unsigned int i = 0; i < *payload_size; i++) {
             if (i == 0) {
-                fprintf(stderr, "0x%x", payload[i]);
+                fprintf(stderr, "0x%x", (uint8_t) payload[i]);
             } else {
-                printf(stderr, ", 0x%x", payload[i]);
+                printf(stderr, ", 0x%x", (uint8_t) payload[i]);
             }
         }
         printf(" ]\n");
